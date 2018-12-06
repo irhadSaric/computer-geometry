e13723bd7d450fce3fd5d308d214ee6fb9d4650f
Irho <irhad.saric@hotmail.com>
2018-12-06 10:03:53 +0100

    clockwise sort

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from point import *
 from vector import Vector
 from simplePoly import *
 from GrahamScan import *
+from sort_clockwise import *
 
 a = [(0, 0), (3, 0), (-1, -3), (-1, 3), (-2, 3), (2, 5), (-1, 1), (-4, 1), (-5, -2)]
 b = [(0, 0), (-1, 1), (-1, -3), (-1, 3), (-2, 3), (2, 5), (-4, 1), (-5, -2), (2, -2), (1, -4), (3, -1)]
+c = [((2, 1), (0, 3)), ((-3, 1), (0, 3)), ((1.5, -1), (-2, -2)) , ((-3, 1), (-2, -2)), ((1.5, -1), (2, 1))]
 
 """if Point.orientation(Point(*a[0]), Point(*a[1]), Point(*a[2])) == 1:
     print("Counter clockwise")
@@ -19,4 +21,5 @@ else:
     print("Don't intersect")"""
 
 #print(simplePoly(a))
-print(GrahamScan(b))
+#print(GrahamScan(b))
+print(sort_cw(c))
---
+++ sort_clockwise.py
@@ -0,0 +1,38 @@
+from point import *
+
+def find_centroid(edges):
+    xCoordinate = 0.0
+    yCoordinate = 0.0
+    for edge in edges:
+        xCoordinate += edge[0][0] + edge[1][0]
+        yCoordinate += edge[0][1] + edge[1][1]
+    return (xCoordinate / len(edges), yCoordinate / len(edges))
+
+def orientate_edges(edges, centroid):
+    for i, edge in enumerate(edges):
+        if Point.orientation(Point(*edge[0]), Point(*edge[1]), Point(*centroid)) == 0:
+            lista = list(edges[i])
+            lista.reverse()
+            edges[i] = tuple(lista)
+
+def construct_vertices_map(edges):
+    map = dict()
+    for edge in edges:
+        map[edge[0]] = edge[1]
+    map[0] = edges[0][0]
+    return map
+
+def traverse_map(verticesMap):
+    sortedVertices = []
+    sortedVertices.append(verticesMap[0])
+    vertex = verticesMap[sortedVertices[0]]
+    while vertex != sortedVertices[0]:
+        sortedVertices.append(vertex)
+        vertex = verticesMap[vertex]
+    return sortedVertices
+
+def sort_cw(edges):
+    centroid = find_centroid(edges)
+    orientate_edges(edges, centroid)
+    verticesMap = construct_vertices_map(edges)
+    return traverse_map(verticesMap)
